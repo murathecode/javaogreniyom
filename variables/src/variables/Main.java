179384dd0ed1d3f5157069e3bb104fcf18b46923
Murat Furkan Çetin <muratfurkancetin@protonmail.com>
2022-09-29 20:41:51 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int ogrenciSayisi = 13;
+	String mesaj = "Öğrenci Sayısı : ";
+	
+	System.out.println(mesaj + ogrenciSayisi);
+	System.out.println("Öğrenci Sayısı : "+ ogrenciSayisi);
+	System.out.println("Öğrenci Sayısı : "+ ogrenciSayisi);
+	System.out.println("Öğrenci Sayısı : "+ ogrenciSayisi);
+	System.out.println("Öğrenci Sayısı : "+ ogrenciSayisi);
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
